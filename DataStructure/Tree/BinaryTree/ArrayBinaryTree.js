9b580c8051e3a2f8a30118aede1dd381047a8dd0
sujan shrestha <tlsujank.co@gmail.com>
2025-06-22 15:44:47 +0545

    feat: add in-order and post-order traversal methods, search, and delete functionality to BinaryTree class

--- DataStructure/Tree/BinaryTree/ArrayBinaryTree.js
+++ DataStructure/Tree/BinaryTree/ArrayBinaryTree.js
@@ -26,6 +26,46 @@ class BinaryTree {
     this.preOrderTraversal(2 * index);
     this.preOrderTraversal(2 * index + 1);
   }
+
+  inOrderTraversal(index = 1) {
+    if (index > this.lastUsedIndex || !this.arr[index]) {
+      return;
+    }
+
+    this.inOrderTraversal(2 * index);
+    console.log(this.arr[index], " ");
+    this.inOrderTraversal(2 * index + 1);
+  }
+
+  postOrderTraversal(index = 1) {
+    if (index > this.lastUsedIndex || !this.arr[index]) {
+      return;
+    }
+
+    this.postOrderTraversal(2 * index);
+    this.postOrderTraversal(2 * index + 1);
+    console.log(this.arr[index], " ");
+  }
+
+  search(element) {
+    for (let i = 1; i < this.arr.length; i++) {
+      if (element === this.arr[i]) {
+        return i;
+      }
+    }
+    return false;
+  }
+
+  delete(element) {
+    let deepestElement = this.arr.at(-1);
+    for (let i = 1; i < this.arr.length; i++) {
+      if (element === this.arr[i]) {
+        this.arr[i] = deepestElement;
+        this.arr.pop();
+      }
+    }
+    return `${element} not found!!`
+  }
 }
 
 const binaryTree = new BinaryTree();
@@ -38,5 +78,7 @@ binaryTree.insert("N5");
 binaryTree.insert("N6");
 binaryTree.insert("N7");
 
-binaryTree.preOrderTraversal();
+binaryTree.postOrderTraversal();
+binaryTree.delete("N2");
+
 console.log(binaryTree);
